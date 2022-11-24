9ea5233b9600eeb4afa8f42aeaf808d750da5c72
Patrick <pcarey@hashicorp.com>
2022-11-24 21:16:22 +1100

    add optional vars

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
 module "mongodeploy" {
   source  = "app.terraform.io/pcarey-demo/mongodeploy/mongodbatlas"
-  version = "1.0.2"
+  version = "latest"
 
+  # Requried variables for module
+  org_id = var.org_id
+
+  # Optional Variables
+  dbuser = var.dbuser
+  database_name = var.database_name
+  dbuser_password = var.dbuser_password
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,19 @@
 variable "org_id" {
   type        = string
-  description = "Id of Mongo organization"
+  description = "MongoDB Organization ID"
+}
+variable "dbuser" {
+  type        = string
+  description = "MongoDB Atlas Database User Name"
+  default = "patrick"
+}
+variable "dbuser_password" {
+  type        = string
+  description = "MongoDB Atlas Database User Password"
+  default = "patricksecure123"
+}
+variable "database_name" {
+  type        = string
+  description = "The database in the cluster to limit the database user to, the database does not have to exist yet"
+  default = "patrickdb"
 }
