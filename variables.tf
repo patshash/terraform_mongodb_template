8f6997b23f11647a929346d865fcad509b7b8761
Patrick <pcarey@hashicorp.com>
2022-11-24 21:33:54 +1100

    update var names

--- variables.tf
+++ variables.tf
@@ -5,15 +5,15 @@ variable "org_id" {
 variable "dbuser" {
   type        = string
   description = "MongoDB Atlas Database User Name"
-  default = "patrick"
+  default = "templateuser"
 }
 variable "dbuser_password" {
   type        = string
   description = "MongoDB Atlas Database User Password"
-  default = "patricksecure123"
+  default = "templatesecure123"
 }
 variable "database_name" {
   type        = string
   description = "The database in the cluster to limit the database user to, the database does not have to exist yet"
-  default = "patrickdb"
+  default = "templatedb"
 }
